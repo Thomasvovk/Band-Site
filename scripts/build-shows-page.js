f291e4452d9d116b363e39fc37b7a5f8b372742c
Thomas Vovk <thomasvovk@icloud.com>
2023-05-09 20:06:01 -0400

    updated bio with get from server shows

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,40 +1,53 @@
-const shows = [
-  {
-    date: "Mon Sept 06 2021",
-    venue: "Ronald Lane",
-    location: "San Francisco, CA",
-  },
+const key = "ac75bb68-cbf2-4fe0-88ca-f67b40e88c1b";
+const url = "https://project-1-api.herokuapp.com/showdates/";
 
-  {
-    date: "Tue Sept 21 2021",
-    venue: "Pier 3 East",
-    location: "San Francisco, CA",
-  },
+function displayEverything() {
+  axios.get(`${url}?api_key=${key}`).then((response) => {
+    response.data.forEach((show) => {
+      displayshows(show);
+    });
+  });
+}
 
-  {
-    date: "Fri Oct 15 2021",
-    venue: "View Lounge",
-    location: "San Francisco, CA",
-  },
+displayEverything();
 
-  {
-    date: "Sat Nov 06 2021",
-    venue: "Hyatt Agency",
-    location: "San Francisco, CA",
-  },
+// const shows = [
+//   {
+//     date: "Mon Sept 06 2021",
+//     venue: "Ronald Lane",
+//     location: "San Francisco, CA",
+//   },
 
-  {
-    date: "Fri Nov 26 2021",
-    venue: "Moscow Center",
-    location: "San Francisco, CA",
-  },
+//   {
+//     date: "Tue Sept 21 2021",
+//     venue: "Pier 3 East",
+//     location: "San Francisco, CA",
+//   },
 
-  {
-    date: "Wed Dec 15 2021",
-    venue: "Press Club",
-    location: "San Francisco, CA",
-  },
-];
+//   {
+//     date: "Fri Oct 15 2021",
+//     venue: "View Lounge",
+//     location: "San Francisco, CA",
+//   },
+
+//   {
+//     date: "Sat Nov 06 2021",
+//     venue: "Hyatt Agency",
+//     location: "San Francisco, CA",
+//   },
+
+//   {
+//     date: "Fri Nov 26 2021",
+//     venue: "Moscow Center",
+//     location: "San Francisco, CA",
+//   },
+
+//   {
+//     date: "Wed Dec 15 2021",
+//     venue: "Press Club",
+//     location: "San Francisco, CA",
+//   },
+// ];
 
 // Structure for Shows Section
 
@@ -55,69 +68,65 @@ concertSection.appendChild(concertsContainer);
 
 // Loop for each Show
 
-function displayshows(shows) {
-  shows.forEach((show) => {
-    const concertSubcontainer = document.createElement("div");
-    concertSubcontainer.classList.add("shows__all-shows");
-    concertsContainer.appendChild(concertSubcontainer);
+function displayshows(show) {
+  const concertSubcontainer = document.createElement("div");
+  concertSubcontainer.classList.add("shows__all-shows");
+  concertsContainer.appendChild(concertSubcontainer);
 
-    const dateInfo = document.createElement("div");
-    dateInfo.classList.add("shows__container");
-    dateInfo.classList.add("shows__date-container");
-    concertSubcontainer.appendChild(dateInfo);
+  const dateInfo = document.createElement("div");
+  dateInfo.classList.add("shows__container");
+  dateInfo.classList.add("shows__date-container");
+  concertSubcontainer.appendChild(dateInfo);
 
-    const dateHeading = document.createElement("h3");
-    dateHeading.classList.add("shows__info-subheader");
-    dateInfo.appendChild(dateHeading);
-    dateHeading.innerText = "DATE";
+  const dateHeading = document.createElement("h3");
+  dateHeading.classList.add("shows__info-subheader");
+  dateInfo.appendChild(dateHeading);
+  dateHeading.innerText = "DATE";
 
-    const dateData = document.createElement("p");
-    dateData.classList.add("shows__date");
-    dateInfo.appendChild(dateData);
-    dateData.innerText = show.date;
+  const dateData = document.createElement("p");
+  dateData.classList.add("shows__date");
+  dateInfo.appendChild(dateData);
+  dateData.innerText = show.date;
 
-    const venueInfo = document.createElement("div");
-    venueInfo.classList.add("shows__container");
-    venueInfo.classList.add("shows__venue-container");
-    concertSubcontainer.appendChild(venueInfo);
+  const venueInfo = document.createElement("div");
+  venueInfo.classList.add("shows__container");
+  venueInfo.classList.add("shows__venue-container");
+  concertSubcontainer.appendChild(venueInfo);
 
-    const venueHeading = document.createElement("h3");
-    venueHeading.classList.add("shows__info-subheader");
-    venueInfo.appendChild(venueHeading);
-    venueHeading.innerText = "VENUE";
+  const venueHeading = document.createElement("h3");
+  venueHeading.classList.add("shows__info-subheader");
+  venueInfo.appendChild(venueHeading);
+  venueHeading.innerText = "VENUE";
 
-    const venueInfoText = document.createElement("p");
-    venueInfoText.classList.add("shows__info");
-    venueInfo.appendChild(venueInfoText);
-    venueInfoText.innerText = show.venue;
+  const venueInfoText = document.createElement("p");
+  venueInfoText.classList.add("shows__info");
+  venueInfo.appendChild(venueInfoText);
+  venueInfoText.innerText = show.place;
 
-    const locationInfo = document.createElement("div");
-    locationInfo.classList.add("shows__container");
-    locationInfo.classList.add("shows__location-container");
-    concertSubcontainer.appendChild(locationInfo);
+  const locationInfo = document.createElement("div");
+  locationInfo.classList.add("shows__container");
+  locationInfo.classList.add("shows__location-container");
+  concertSubcontainer.appendChild(locationInfo);
 
-    const locationHeading = document.createElement("h3");
-    locationHeading.classList.add("shows__info-subheader");
-    locationInfo.appendChild(locationHeading);
-    locationHeading.innerText = "LOCATION";
+  const locationHeading = document.createElement("h3");
+  locationHeading.classList.add("shows__info-subheader");
+  locationInfo.appendChild(locationHeading);
+  locationHeading.innerText = "LOCATION";
 
-    const locationText = document.createElement("p");
-    locationText.classList.add("shows__info");
-    locationInfo.appendChild(locationText);
-    locationText.innerText = show.location;
+  const locationText = document.createElement("p");
+  locationText.classList.add("shows__info");
+  locationInfo.appendChild(locationText);
+  locationText.innerText = show.location;
 
-    const showsButton = document.createElement("div");
-    showsButton.classList.add("shows__button-container");
-    concertSubcontainer.appendChild(showsButton);
+  const showsButton = document.createElement("div");
+  showsButton.classList.add("shows__button-container");
+  concertSubcontainer.appendChild(showsButton);
 
-    const buyTickets = document.createElement("button");
-    buyTickets.classList.add("shows__buy-button");
-    showsButton.appendChild(buyTickets);
-    buyTickets.innerText = "BUY TICKETS";
-  });
+  const buyTickets = document.createElement("button");
+  buyTickets.classList.add("shows__buy-button");
+  showsButton.appendChild(buyTickets);
+  buyTickets.innerText = "BUY TICKETS";
 }
-
-displayshows(shows);
 
 // clicking on a individual row make that row "selected"
 // the row should stay selected until another row is clicked
